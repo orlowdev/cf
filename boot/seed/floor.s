01705aaeb5086d8e0126d99bc045bb62677599e4
谢尔盖 ||↓ <sergei@orlow.dev>
2026-08-19 12:42:58 +0300

    Enhance the root page by making it an elastic reserve-in-place bump

--- boot/seed/floor.s
+++ boot/seed/floor.s
@@ -6,26 +6,55 @@ _start:
 	mov x16, #1
 	svc #0x80
 .globl _cf_arena_init
+.globl _cf_grow
 .globl _cf_oom
 .p2align 2
 _cf_arena_init:
 	mov x0, #0
-	mov x1, #0x10000000
-	mov x2, #3
+	mov x1, #0x100000000
+	mov x2, #0
 	mov x3, #0x1002
 	mov x4, #-1
 	mov x5, #0
 	mov x16, #197
 	svc #0x80
 	b.cs _cf_mmap_fail
+	mov x12, x0
 	adrp x9, _cf_top@PAGE
 	add x9, x9, _cf_top@PAGEOFF
-	str x0, [x9]
-	mov x1, #0x10000000
-	add x11, x0, x1
+	str x12, [x9]
+	mov x1, #0x100000000
+	add x11, x12, x1
 	adrp x10, _cf_limit@PAGE
 	add x10, x10, _cf_limit@PAGEOFF
 	str x11, [x10]
+	mov x0, x12
+	mov x1, #0x10000000
+	mov x2, #3
+	mov x16, #74
+	svc #0x80
+	b.cs _cf_mmap_fail
+	mov x1, #0x10000000
+	add x13, x12, x1
+	adrp x14, _cf_committed@PAGE
+	add x14, x14, _cf_committed@PAGEOFF
+	str x13, [x14]
+	ret
+_cf_grow:
+	adrp x9, _cf_committed@PAGE
+	add x9, x9, _cf_committed@PAGEOFF
+	ldr x10, [x9]
+	mov x11, #0x10000000
+	sub x12, x11, #1
+	add x13, x0, x12
+	bic x14, x13, x12
+	sub x1, x14, x10
+	mov x0, x10
+	mov x2, #3
+	mov x16, #74
+	svc #0x80
+	b.cs _cf_mmap_fail
+	str x14, [x9]
 	ret
 _cf_mmap_fail:
 	mov x0, #71
